4191326748ef08ebb069190524794e92b815cbdf
AkhiAkther <akhiakther0022@gmail.com>
2023-10-13 23:56:36 +0600

    sttyle added

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -5,7 +5,7 @@ import Title from './Title'
 const AboutMe = () => {
   return (
   <section id='aboutme'>
-    <Container>
+    <Container className='border border-zinc-400 bg-zinc-100 p-4'>
       <Title title='About Me'/>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias ducimus illo assumenda quas voluptatibus accusantium, voluptates officia voluptatem, beatae saepe atque eligendi? Delectus perferendis, dignissimos commodi atque doloribus fuga exercitationem ipsum officia totam similique aut voluptatem possimus minima at libero illo quos, quod earum deserunt! Aut vel maiores iste laboriosam!</p>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat reiciendis laborum numquam quidem aspernatur asperiores rerum tempora debitis officia fuga?</p>
